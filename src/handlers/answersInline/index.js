e923bd741f561ceccba57b410a819de1af695888
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-06 01:17:44 +0300

    chore(answers check): test update

--- src/handlers/answersInline/index.js
+++ src/handlers/answersInline/index.js
@@ -1,3 +1,4 @@
+const Extra = require("telegraf/extra");
 const Table = require("../../helpers/table");
 
 const answers = ["table::1::1", "table::2::2", "table::4::3"];
@@ -6,5 +7,11 @@ module.exports = () => [
   "check_ans",
   async (ctx) => {
     ctx.answerCbQuery("📚 Ответы на тест 23:\n  - 1A\n  - 2Б\n  - 3Г", true);
+    ctx.reply(
+      "Правильно - Вот балл получи",
+      Extra.HTML().markup((m) =>
+        m.inlineKeyboard([m.callbackButton("Загрузить новый тест", "create_table")])
+      )
+    );
   },
 ];
