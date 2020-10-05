9bae7a0c9249f9836517c20ea4ade5c330653760
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-06 01:23:43 +0300

    feat(start cmd): checking user in db and handling errors

--- src/handlers/start/index.js
+++ src/handlers/start/index.js
@@ -1,5 +1,22 @@
 const { mainMenu } = require("../../helpers");
+const User = require("../../models/User");
 
 module.exports = () => async (ctx) => {
-  await ctx.replyWithMarkdown("Добро пожаловать", { reply_markup: { inline_keyboard: mainMenu } });
+  try {
+    // Поиск пользователя в базе данных
+    const user = await User.findOne({ id: ctx.session.id }).cache();
+    if (!user) {
+      await User.create({ id: ctx.session.id });
+    }
+    await ctx.replyWithMarkdown("Добро пожаловать", {
+      reply_markup: { inline_keyboard: mainMenu },
+    });
+  } catch (err) {
+    ctx.replyWithHTML(
+      "<b><code>[⚠️ | Ошибка]</code>\nПроизошла неизвестная ошибка при отправке сообщения." +
+        "Попробуйте еще раз. Если ошибка не пропала, свяжитесь с нами. /feedback</b>"
+    );
+
+    console.error(`[%s] Произошла ошибка при создании таблицы.`, new Date().toTimeString(), err);
+  }
 };
