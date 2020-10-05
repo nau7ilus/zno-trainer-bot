6caa7641cd14925dd2a7e5debe05c2ca31490273
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-06 01:23:59 +0300

    feat(db models): added task model

---
+++ src/models/Task.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const TaskSchema = new Schema(
+  {
+    type: { type: String, required: true },
+    photoURL: { type: String, required: true },
+    answers: { type: Schema.Types.Mixed, required: true },
+    ids: {
+      test: Number,
+      task: Number,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = model("task", TaskSchema);
