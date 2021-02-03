917b564059748c8f079db3f4007e0f299b01007b
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2021-02-03 08:29:48 +0200

    chore(select): linted

--- src/handlers/Tasks/select.js
+++ src/handlers/Tasks/select.js
@@ -15,29 +15,42 @@ module.exports = class extends Handler {
 
   run(ctx) {
     if (ctx.match[1] === 'subject') {
-      send(ctx, ctx.i18n.t('select.subjects.message'), [...selectSubject(ctx), backButton(ctx, 'start')]);
+      send(ctx, ctx.i18n.t('select.subjects.message', { secret: ctx.match[0] }), [
+        ...selectSubject(ctx),
+        backButton(ctx, 'start'),
+      ]);
     }
 
     if (ctx.match[1] === 'types') {
-      send(ctx, ctx.i18n.t('select.types.message'), [
-        ...selectTypes(ctx, ctx.match[2]),
-        backButton(ctx, 'select::subject'),
-      ]);
+      send(
+        ctx,
+        ctx.i18n.t('select.types.message', {
+          secret: ctx.match[0],
+        }),
+        [...selectTypes(ctx, ctx.match[2]), backButton(ctx, 'select::subject')],
+      );
     }
 
     if (ctx.match[1] === 'themes') {
       const themes = getThemes(ctx)[ctx.match[2]];
       if (!ctx.match[3]) {
-        send(ctx, ctx.i18n.t('select.themes.message', { themes: Object.keys(themes) }), [
-          ...chunk(
-            Object.keys(themes).map((t, i) => ({
-              text: `🔸 ${i + 1}`,
-              callback_data: `select::themes::${ctx.match[2]}::${i}`,
-            })),
-            4,
-          ),
-          backButton(ctx, `select::types::${ctx.match[2]}`),
-        ]);
+        send(
+          ctx,
+          ctx.i18n.t('select.themes.message', {
+            secret: ctx.match[0],
+            themes: Object.keys(themes),
+          }),
+          [
+            ...chunk(
+              Object.keys(themes).map((t, i) => ({
+                text: `🔸 ${i + 1}`,
+                callback_data: `select::themes::${ctx.match[2]}::${i}`,
+              })),
+              4,
+            ),
+            backButton(ctx, `select::types::${ctx.match[2]}`),
+          ],
+        );
       } else {
         const markup = [
           ...chunk(
@@ -50,7 +63,14 @@ module.exports = class extends Handler {
           backButton(ctx, `select::themes::${ctx.match[2]}`),
         ];
 
-        send(ctx, ctx.i18n.t('select.themes.message', { themes: Object.values(themes)[ctx.match[3]] }), markup);
+        send(
+          ctx,
+          ctx.i18n.t('select.themes.message', {
+            themes: Object.values(themes)[ctx.match[3]],
+            secret: ctx.match[0],
+          }),
+          markup,
+        );
       }
     }
   }
