a918d1182524c65a13037919a8ea50efd02df3ac
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2021-02-03 08:28:13 +0200

    chore: added enums

---
+++ src/helpers/enums.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Subjects = {
+  Unknown: 0,
+  Algebra: 1,
+  Geometry: 2,
+};
+
+const Tasks = {
+  Base: 0,
+  Table: 1,
+  Open: 2,
+  Structurized: 3,
+};
+
+module.exports = { Subjects, Tasks };
