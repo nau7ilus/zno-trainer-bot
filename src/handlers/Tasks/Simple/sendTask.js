d950e7fd6451f2c6da25ca8bb64eef712a6ee6f2
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-02-28 22:34:55 +0200

    fix(sendSimpleTask): send 5 buttons

--- src/handlers/Tasks/Simple/sendTask.js
+++ src/handlers/Tasks/Simple/sendTask.js
@@ -13,11 +13,11 @@ module.exports = class extends Handler {
   run(ctx, task) {
     const photoURL = `${process.env.CDN_URL}/${task.id}/task.png`;
     ctx.replyWithPhoto(photoURL, {
-      caption: ctx.i18n.t('tasks.simple.question'),
+      // Caption: ctx.i18n.t('tasks.simple.question'),
       parse_mode: 'HTML',
       reply_markup: {
         inline_keyboard: [
-          new Array(4).fill('').map((i, j) => ({
+          new Array(5).fill('').map((i, j) => ({
             text: `🔸 ${alphabet[j]}`,
             callback_data: `checkSimple::${j}`,
           })),
