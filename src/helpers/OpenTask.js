a28446fa517159e5639d86ea3217173be30f1361
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-02-28 22:27:29 +0200

    feat(opentask): added new type of tasks

---
+++ src/helpers/OpenTask.js
@@ -0,0 +1,109 @@
+'use strict';
+
+module.exports = class OpenTask {
+  constructor(ctx, task = {}, markup = {}) {
+    this.ctx = ctx;
+    this.message = ctx.update.callback_query.message;
+
+    this.task = task;
+    this.markup = markup?.reply_markup?.inline_keyboard ?? markup;
+  }
+
+  get givenAnswers() {
+    return this.markup[0].map(i => (isNaN(+i.text) ? null : +i.text));
+  }
+
+  get keyboard() {
+    return {
+      reply_markup: {
+        inline_keyboard: this.markup,
+      },
+    };
+  }
+
+  get taskAnswers() {
+    return this.task.taskType === 2 ? [this.task.answer] : this.task.answer;
+  }
+
+  createMarkup() {
+    this.markup = [
+      new Array(this.taskAnswers.length)
+        .fill(this.ctx.i18n.t('tasks.open.noAnswer'))
+        .map((text, i) => ({ text, callback_data: `selectAnswer::${i}::${this.task.id}` })),
+
+      [
+        { text: this.ctx.i18n.t('tasks.skip.title'), callback_data: 'skip' },
+        { text: this.ctx.i18n.t('tasks.open.checkAnswers'), callback_data: `checkOpenTask::${this.task.id}` },
+      ],
+    ];
+
+    return this.markup;
+  }
+
+  selectAnswer(index) {
+    this.ctx.session.currentAnswer = index;
+    this.ctx.session.pastValue = !isNaN(+this.markup[0][index].text) ? this.markup[0][index].text : null;
+
+    this.markup[0][+index].text = this.ctx.i18n.t('tasks.open.selectedAnswer');
+    this.markup[0][+index].callback_data = `unselectAnswer::${index}::${this.task.id}`;
+
+    return this;
+  }
+
+  unselectAnswer(index) {
+    this.ctx.session.currentAnswer = undefined;
+    this.ctx.session.pastValue = undefined;
+
+    this.markup[0][+index].text = this.ctx.session.pastValue ?? this.ctx.i18n.t('tasks.open.noAnswer');
+    this.markup[0][+index].callback_data = `selectAnswer::${index}::${this.task.id}`;
+
+    return this;
+  }
+
+  validMessage(text) {
+    const args = text.split(/ +/g);
+    if (args.length > this.taskAnswers.length) return 'errors.tasks.open.tooManyArguments';
+
+    const invalidArgs = args.filter(arg => isNaN(+arg));
+    if (invalidArgs.length > 0) return 'errors.tasks.open.invalidArgument';
+
+    return true;
+  }
+
+  setAnswer(index, value) {
+    this.markup[0][index].text = value;
+    this.markup[0][index].callback_data = `selectAnswer::${index}::${this.task.id}`;
+
+    this.ctx.session.currentAnswer = undefined;
+    this.ctx.session.pastValue = undefined;
+
+    return this;
+  }
+
+  parseAnswers() {
+    const givenAnswers = this.markup[0].map(i => +i.text);
+
+    if (givenAnswers.filter(i => isNaN(i)).length > 0) {
+      throw new Error('errors.tasks.open.noAnswers');
+    }
+
+    const correctAnswers = [];
+    givenAnswers.forEach(ans => {
+      if (this.taskAnswers.includes(+ans)) correctAnswers.push(+ans);
+    });
+
+    return correctAnswers;
+  }
+
+  deactivateButtons() {
+    this.markup.pop();
+    this.markup[0].forEach(i => (i.callback_data = 'blank'));
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      task: this.task,
+    };
+  }
+};
---
+++ src/scenes/checkOpenTask.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const TelegrafScene = require('telegraf/scenes/base');
+const OpenTask = require('../helpers/OpenTask');
+const Scene = require('../structures/pieces/Scene');
+
+module.exports = class extends Scene {
+  constructor(...args) {
+    super(...args, {
+      name: 'checkOpenTask',
+    });
+  }
+  run() {
+    const editMessageMarkup = (ctx, newMarkup) => {
+      const { message } = ctx.session.openTaskData;
+      ctx.telegram.editMessageReplyMarkup(message.chat.id, message.message_id, null, newMarkup);
+    };
+
+    const scene = new TelegrafScene('checkOpenTask');
+    scene.enter(ctx => {
+      ctx.replyWithHTML(
+        ctx.i18n.t('tasks.open.listenAnswer.enter', {
+          index: ctx.session.currentAnswer,
+          pastValue: ctx.session.pastValue,
+        }),
+      );
+
+      const { message, task } = ctx.session.openTaskData;
+      const openTask = new OpenTask(ctx, task, message);
+
+      ctx.scene.session.openTask = openTask;
+      ctx.scene.session.sceneWillDie = Date.now() + 30 * 1000;
+      setTimeout(() => ctx.scene.leave(), 30 * 1000);
+    });
+
+    scene.leave(ctx => {
+      if (ctx.scene.session.timeLeft) {
+        ctx.scene.session.openTask.unselectAnswer(ctx.match[1]);
+        editMessageMarkup(ctx, ctx.session.openTask.keyboard.reply_markup);
+        ctx.replyWithHTML(ctx.i18n.t('errors.tasks.open.timeLeft'));
+      }
+    });
+
+    scene.on('text', ctx => {
+      if (ctx.scene.session.sceneWillDie < Date.now()) return ctx.scene.leave();
+
+      if (!ctx.message) {
+        ctx.scene.session.openTask.unselectAnswer(ctx.match[1]);
+        editMessageMarkup(ctx, ctx.scene.session.openTask.keyboard.reply_markup);
+        return ctx.replyWithHTML(ctx.i18n.t('errors.tasks.open.timeLeft'));
+      }
+
+      const givenAnswer = ctx.message.text;
+      const validAnswer = ctx.scene.session.openTask.validMessage(givenAnswer);
+      if (typeof validAnswer === 'string') {
+        ctx.scene.session.openTask.unselectAnswer(ctx.match[1]);
+        editMessageMarkup(ctx, ctx.scene.session.openTask.keyboard.reply_markup);
+        return ctx.replyWithHTML(ctx.i18n.t(validAnswer));
+      }
+
+      ctx.scene.session.openTask.setAnswer(ctx.session.currentAnswer, givenAnswer);
+      editMessageMarkup(ctx, ctx.scene.session.openTask.keyboard.reply_markup);
+      return ctx.scene.leave();
+    });
+
+    scene.on('message', ctx => {
+      ctx.scene.session.timeLeft = true;
+      ctx.scene.leave();
+    });
+
+    return scene;
+  }
+};
