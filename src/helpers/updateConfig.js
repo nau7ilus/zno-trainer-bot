cddf877a3019a6f50ce13ffb3a1fe5a3bd1cdd6d
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-02-28 22:27:50 +0200

    chore(updateConfig): added try catch

--- src/helpers/updateConfig.js
+++ src/helpers/updateConfig.js
@@ -5,9 +5,13 @@ const axios = require('axios');
 module.exports = async bot => {
   if (!process.env.CDN_URL) throw new Error('Не указана ссылка на сайт CDN');
 
-  const res = await axios.get(`${process.env.CDN_URL}/config.json`);
-  if (!res.data) throw new Error('Ошибка при запросе конфига');
+  try {
+    const res = await axios.get(`${process.env.CDN_URL}/config.json`);
+    if (!res.data) throw new Error('Ошибка при запросе конфига');
 
-  bot.context.tasks = res.data;
-  console.log('[Config] Конфиг бота успешно обновлен.');
+    bot.context.tasks = res.data;
+    console.log('[Config] Конфиг бота успешно обновлен.');
+  } catch (err) {
+    console.error(err);
+  }
 };
